5aa83cff69b738f6edbb15e0a9bbf9e93e3c41c9
rishinandigam <gnyanarushi@gmail.com>
2024-06-06 15:22:44 +0530

    mod

--- src/rishi.java
+++ src/rishi.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class rishi {
+
+public class rishi {
+        public  void display()
+         {
+             System.out.println("Rishi");
+         }
+    public static void main(String[] args) {
+        System.out.println(" Hello world ");
+        System.out.println("Welcome ton java !");
+        rishi obj = new rishi();
+        obj.display();
+    }
 }
